662917983bdf3e461ee808584233185f302a6731
olilliterate <oliver@bjerg.co.uk>
2026-03-10 14:55:21 +0000

    Fixed bug in cors line

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const fruitsApp = express();
 const fruitRoutes = require("./routes/fruitRoutes.js")
 
-application.use(cors())
+fruitsApp.use(cors())
 fruitsApp.use(express.json()); //any http method
 fruitsApp.use("/fruits", fruitRoutes)
 
